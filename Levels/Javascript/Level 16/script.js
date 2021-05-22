bf48cb9a438886ff89114de3c012756503061e5b
SpaansDamian <78344352+SpaansDamian@users.noreply.github.com>
2021-05-22 16:56:11 +0200

    Add files via upload

---
+++ Levels/Javascript/Level 16/script.js
@@ -0,0 +1,20 @@
+// Opdracht A
+const ikRockArrowFunctions = () => {
+    console.log("Joe, ik rock de arrow functions!");
+};
+
+ikRockArrowFunctions();
+
+// Opdracht B
+const fivePlusSeven = () => 5 + 7;
+
+fivePlusSeven();
+
+// Opdracht C
+const myFunction = (a, b) => a + b;
+
+// Opdracht D
+const addFive = a => a + 5;
+
+// Opdracht E
+const createObject = () => ({ value: "hoi" });
